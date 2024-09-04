31bdc050fba8c0318371f267bbcbaf0e1e2856b6
RonaldGeneral <gogo23993@gmail.com>
2024-09-04 13:15:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cglkhpyrvgqq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tehcs-employee"
+customregion = "us-east-1"
+
